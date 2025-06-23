e72802abb27a498b5907086148bf487373fe138c
Hunter Casten <huntercasten@icloud.com>
2025-06-22 21:37:06 -0700

    try out jj

--- bin/install_tools.sh
+++ bin/install_tools.sh
@@ -7,5 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 curl -fsSL https://tailscale.com/install.sh | sh
 
 
-cargo install bat exa ripgrep du-dust zellij mprocs irust bacon cargo-info ncspot porsmo speedtest-rs rtx-cli starship wiki-tui atuin
-
+cargo install bat eza ripgrep du-dust zellij mprocs irust bacon cargo-info ncspot porsmo speedtest-rs rtx-cli starship wiki-tui atuin
+cargo install --locked --bin jj jj-cli
