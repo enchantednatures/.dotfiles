613c549541cf09f78bc8f7469678796cb9802d12
Hunter Casten <huntercasten@icloud.com>
2025-07-09 20:06:33 -0700

    .

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -3,7 +3,7 @@ local M = {}
 require "utils.sops"
 require "utils.kubernetes"
 require "utils.diff"
-require "utils.ai"
+
 local diagnostics = require "utils.diagnostics"
 diagnostics:setup_diagnostics()
 
