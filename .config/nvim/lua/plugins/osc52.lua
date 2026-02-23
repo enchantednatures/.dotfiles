9777cdceddaa1526d7e59710e7d2c79d300fb7a7
Hunter Casten <huntercasten@icloud.com>
2026-02-23 00:06:58 +0000

    wip

--- .config/nvim/lua/plugins/osc52.lua
+++ .config/nvim/lua/plugins/osc52.lua
@@ -1,17 +1,40 @@
+local function is_remote()
+  -- return vim.fn.exists("$SSH_CONNECTION") == 1 or vim.fn.exists("$SSH_CLIENT") == 1 or vim.fn.exists("$SSH_TTY") == 1
+  return true
+end
+
 return {
-  "ojroques/vim-oscyank",
+  "ojroques/nvim-osc52",
+
   event = "VeryLazy",
-  enabled = false,
-  dependencies = {
-    "ojroques/nvim-osc52",
-  },
+  enabled = is_remote() or vim.fn.empty(vim.fn.getenv "DISPLAY") == 1 or vim.fn.exists "$ZELLIJ_SESSION_NAME" == 1,
   config = function()
-    vim.api.nvim_set_option("clipboard", "unnamedplus")
+    local osc52 = require "osc52"
+
+    vim.g.clipboard = {
+      name = "osc52",
+      copy = {
+        ["+"] = function(lines, regtype)
+          osc52.copy "+"
+          return vim.fn.setreg("+", lines, regtype)
+        end,
+        ["*"] = function(lines, regtype)
+          osc52.copy "*"
+          return vim.fn.setreg("*", lines, regtype)
+        end,
+      },
+      paste = {
+        ["+"] = function() return vim.fn.getreg "+" end,
+        ["*"] = function() return vim.fn.getreg "*" end,
+      },
+      cache_enabled = 0,
+    }
+
+    vim.opt.clipboard = "unnamed,unnamedplus"
     vim.api.nvim_create_autocmd("TextYankPost", {
-      group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+      group = vim.api.nvim_create_augroup("osc52_highlight", { clear = true }),
       callback = function()
-        vim.highlight.on_yank { higroup = "IncSearch", timeout = 500 }
-        if vim.v.event.operator == "y" and vim.v.event.regname == "" then vim.cmd [[OSCYankRegister]] end
+        if vim.v.event.operator == "y" then osc52.copy "+" end
       end,
     })
   end,
